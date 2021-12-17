b99eae8e65228c973e26d1d30c8ec968681beb18
Julien Dubois <julien.dubois@gmail.com>
2021-12-17 13:55:11 +0100

    Add runtime types

---
+++ src/main/java/io/github/nubesgen/cli/configuration/RuntimeType.java
@@ -0,0 +1,26 @@
+package io.github.nubesgen.cli.configuration;
+
+/**
+ * Runtimes supported.
+ * <p>
+ * Those can also be grouped in the following categories:
+ * <p>
+ * DOCKER = DOCKER, DOCKER_SPRING
+ * JAVA = JAVA, JAVA_GRADLE, SPRING, SPRING_GRADLE, QUARKUS
+ * SPRING = DOCKER_SPRING, SPRING, SPRING_GRADLE
+ * MAVEN = DOCKER_SPRING, JAVA, SPRING, QUARKUS
+ * GRADLE = JAVA_GRADLE, SPRING_GRADLE
+ * QUARKUS = QUARKUS
+ */
+public enum RuntimeType {
+    DOCKER,
+    DOCKER_SPRING,
+    JAVA,
+    JAVA_GRADLE,
+    SPRING,
+    SPRING_GRADLE,
+    QUARKUS,
+    QUARKUS_NATIVE,
+    DOTNET,
+    NODEJS,
+}
